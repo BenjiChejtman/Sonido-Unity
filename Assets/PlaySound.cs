c1b3ecebe01210866ee91645ac313a3361f0d8f0
BenjiChejtman <46895375@est.ort.edu.ar>
2022-05-31 11:59:02 -0300

    Initial commit

---
+++ Assets/PlaySound.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour {
+
+    public AudioClip GunSound;
+    public AudioClip SwordSound;
+
+    AudioSource FuenteAudio;
+
+	// Use this for initialization
+	void Start ()
+    {
+        FuenteAudio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.E))
+        {
+            FuenteAudio.clip = GunSound;
+            FuenteAudio.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            FuenteAudio.clip = SwordSound;
+            FuenteAudio.Play();
+        }
+    }
+}
+
